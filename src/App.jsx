030e3b0263d54e34ac1899cc79de91c1923af18e
nicoleiii <nagitookomaeda100@gmail.com>
2025-11-28 21:49:06 -0500

    Making it pretty

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="desktop-container">
+      <div className="glow-left"></div>
+      <div className="glow-right"></div>
+
+      <div className="content">
+        <h1 className="title">Inkling AI</h1>
+        <p className="subtitle">
+          Smart, AI-Powered Quiz Generation for Learning Anything in Seconds.
         </p>
+
+        <button className="start-btn">Start Learning</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
